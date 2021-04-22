360d686668df66ae5fc80b556ff116e7fb00ea36
ldalmeida <nicole-silva-pedrosa@hotmail.com>
2021-04-22 15:45:31 -0300

    upload do projeto pro github

---
+++ Trabalho BD1/Code/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Trabalho_BD1.Code;
+namespace Trabalho_BD1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
